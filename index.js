c094064ed3a68e67a320cd041d0058aebec29566
Alex Johnson <alex@johnsonwebware.com>
2019-10-20 14:37:42 -0700

    express-handlebars and some exmple details
    Signed-off-by: Alex Johnson <alex@johnsonwebware.com>

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const dotenv = require('dotenv');
-
-const app = express();
 dotenv.config();
 
-app.get('/', (req, res) => res.send('hello World'));
+const app = express();
+app.engine('.hbs', exphbs({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+app.get('/', (req, res) => res.render('home'));
 
 app.listen(process.env.PORT, () => console.log(`housetrack running on port ${process.env.PORT}`))
